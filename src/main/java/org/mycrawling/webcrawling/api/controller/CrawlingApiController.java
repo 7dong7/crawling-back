3e5847b440d5b36f9851330cb08378cded964bb2
7dong7 <tmdkdl777@gmail.com>
2025-04-07 22:09:43 +0900

    기사 단어 빈도수 확인하기 위한 KOMORAN 형태소 분석기 의존성 추가

--- src/main/java/org/mycrawling/webcrawling/api/controller/CrawlingApiController.java
+++ src/main/java/org/mycrawling/webcrawling/api/controller/CrawlingApiController.java
@@ -1,5 +1,9 @@
 package org.mycrawling.webcrawling.api.controller;
 
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.KomoranResult;
+import kr.co.shineware.nlp.komoran.model.Token;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mycrawling.webcrawling.domain.dto.News;
@@ -9,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,12 +33,33 @@ public class CrawlingApiController {
         log.info("웹 크롤링 실행");
         Map<String, List<News>> map = new HashMap<>();
 
-
+        // 각 포털의 뉴스 가져오기
         List<News> naverNews = crawlingService.crawlingNaverNews();
         List<News> daumNews = crawlingService.crawlingDaumNews();
         List<News> googleNews = crawlingService.crawlingGoogleNews();
         List<News> nateNews = crawlingService.crawlingNateNews();
 
+        // 각 뉴스제목의 핵심 키워드 작성
+        List<News> allNews = new ArrayList<>();
+        allNews.addAll(naverNews);
+        allNews.addAll(daumNews);
+        allNews.addAll(googleNews);
+        allNews.addAll(nateNews);
+
+        Map<String, Integer> keywordFrequency = new HashMap<>(); // 단어 빈도수 저장
+
+        Komoran komoran = new Komoran(DEFAULT_MODEL.FULL);
+
+        for (News news : allNews) {
+            System.out.println("======================= 뉴스 하나 ==============");
+            KomoranResult analyze = komoran.analyze(news.getTitle()); // 분석할 문장 넣기
+            String plainText = analyze.getPlainText();
+            System.out.println("plainText = " + plainText);
+            List<Token> tokenList = analyze.getTokenList();
+            for (Token token : tokenList) {
+                System.out.println("token = " + token);
+            }
+        }
 
 
         map.put("naver", naverNews);
